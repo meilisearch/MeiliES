8877755f0cf9ac6ab745476424d4badf38c70020
Clément Renault <renault.cle@gmail.com>
2019-03-02 18:29:56 +0100

    feat: Introduce a basic server

--- meilies-server/src/main.rs
+++ meilies-server/src/main.rs
@@ -1,19 +1,11 @@
-use std::{env, str};
-use std::time::{Instant, Duration};
-use std::io::BufReader;
+use std::env;
+use std::time::Instant;
 
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use futures::sync::mpsc::{channel, Sender, Receiver};
-use futures::future::poll_fn;
-use tokio::io::{self, lines, write_all};
 use tokio::net::TcpListener;
-use tokio::timer::Interval;
 use tokio::prelude::*;
-use tokio::codec::{Encoder, Decoder};
-use tokio::sync::mpsc;
-use bytes::{BufMut, BytesMut};
+use tokio::codec::Decoder;
 use log::{info, error};
-use sled::{Db, Event};
+use sled::Db;
 
 use meilies::codec::{RespCodec, RespValue, RespMsgError};
 
@@ -41,81 +33,22 @@ fn main() {
             let framed = RespCodec::default().framed(socket);
             let (writer, reader) = framed.split();
 
-            let db = db.clone();
-            let responses = reader.map(move |line| {
+            let _db = db.clone();
+            let responses = reader.map(|value| {
 
-                Ok(line)
+                println!("{:?}", value);
 
-                // let mut parts = line.split(|c| c == ' ' || c == '\n');
-
-                // match (parts.next(), parts.next()) {
-                //     (Some("SUBSCRIBE"), Some(stream_name)) => {
-
-                //         println!("subscription to {:?}", stream_name);
-
-                //         let db = db.clone();
-                //         let stream_name = stream_name.as_bytes().to_vec();
-                //         let (mut tx, rx) = mpsc::channel(100);
-
-                //         tokio::spawn(poll_fn(move || {
-                //             let stream_name = stream_name.clone(); // o_O wtf !!!?
-                //             tokio_threadpool::blocking(|| {
-                //                 let tree = db.open_tree(stream_name).unwrap();
-                //                 for result in tree.iter() {
-                //                     let (k, v) = result.unwrap();
-                //                     tx.try_send((k, v.to_vec())).unwrap();
-                //                 }
-                //                 for event in tree.watch_prefix(vec![]) {
-                //                     if let Event::Set(k, v) = event {
-                //                         tx.try_send((k, v.to_vec())).unwrap();
-                //                     }
-                //                 }
-                //             }).map_err(|e| error!("{}", e))
-                //         }));
-
-                //         (line, Some(rx))
-                //     },
-                //     (Some("PUBLISH"), Some(stream_name)) => {
-                //         if let Some(content) = parts.next() {
-
-                //             println!("publishing to {:?}", stream_name);
-
-                //             let stream_name = stream_name.as_bytes().to_vec();
-                //             let tree = db.open_tree(stream_name).unwrap();
-
-                //             let unique_id = db.generate_id().unwrap() as u64;
-                //             let mut unique_id_buff = Vec::new();
-                //             let _ = unique_id_buff.write_u64::<BigEndian>(unique_id);
-
-                //             let content = content.as_bytes().to_vec();
-                //             tree.set(unique_id_buff, content).unwrap();
-                //         }
-
-                //         (line, None)
-                //     }
-                //     _ => {
-                //         // TODO return a real error
-                //         panic!("whoops: {:?}", line)
-                //     },
-                // }
+                Ok(value)
             });
 
-            let writes = responses.fold(writer, |writer, response: Result<RespValue, RespMsgError>| {
-                // match rx {
-                //     Some(rx) => {
-                //         let keys_values = rx
-                //             .map(|(k, v)| {
-                //                 let key = k.as_slice().read_u64::<BigEndian>().unwrap();
-                //                 let value = str::from_utf8(&v).unwrap();
-                //                 format!("{} -- {}", key, value)
-                //             })
-                //             .map_err(|e| std::io::ErrorKind::Interrupted);
-                //         writer.send_all(keys_values).map(|(s, _)| s).boxed()
-                //     },
-                //     None => writer.send(response).boxed(),
-                // }
+            let writes = responses.fold(writer, |writer, value: Result<RespValue, RespMsgError>| {
 
-                writer.send(response.unwrap())
+                println!("{:?}", value);
+
+                match value {
+                    Ok(value) => writer.send(value),
+                    Err(_e) => writer.send(RespValue::Error("Whoops an error occured!".to_owned())),
+                }
             });
 
             let msg = writes.then(|_| Ok(()));
