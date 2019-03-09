6ba06bd48b2dce2ecdbb3a6ca142590ef5b4ef2b
Clément Renault <renault.cle@gmail.com>
2019-03-09 18:55:53 +0100

    feat: Introduce a basic subscribe client

--- meilies-client/src/main.rs
+++ meilies-client/src/main.rs
@@ -1,12 +1,13 @@
+use std::str::FromStr;
 use std::net::ToSocketAddrs;
 
 use log::error;
 use structopt::StructOpt;
-use tokio::codec::Decoder;
-use tokio::net::TcpStream;
 use tokio::prelude::*;
+use futures::stream::Stream;
 
-use meilies::codec::{RespValue, RespMsgError, RespCodec};
+use meilies_client::sub_connect;
+use meilies::stream::Stream as EsStream;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "meilies-cli", about = "A basic cli for MeiliES.")]
@@ -34,51 +35,23 @@ fn main() {
         Err(e) => return error!("error parsing addr; {}", e),
     };
 
-    let socket = TcpStream::connect(&addr);
-
-    let client = socket
-        .map_err(|e| error!("error accepting socket; {}", e))
-        .and_then(|socket| {
-            let framed = RespCodec::default().framed(socket);
-            let (writer, reader) = framed.split();
-
-            let must_listen = match opt.cmd_args.get(0) {
-                Some(s) if s == "subscribe" => true,
-                _ => false,
-            };
-
-            let args = opt.cmd_args.into_iter().map(RespValue::bulk_string).collect();
-            let command = RespValue::Array(args);
-
-            let writes = writer.send(command);
-
-            let reader = if must_listen {
-                Box::new(reader) as Box<Stream<Item=_, Error=_> + Send>
-            } else {
-                Box::new(reader.take(1)) as Box<Stream<Item=_, Error=_> + Send>
-            };
-
-            let messages = reader.for_each(|value| {
-                match value {
-                    RespValue::SimpleString(ref s) if s == "OK" => {
-                        println!("received OK");
-                        Ok(())
-                    }
-                    RespValue::Error(e) => {
-                        eprintln!("error: {}", e);
-                        Err(RespMsgError::SimpleStringContainCrlf)
-                    },
-                    other => {
-                        println!("received: {:?}", other);
-                        Ok(())
-                    }
-                }
-            }).then(|_| Ok(()));
-
-            tokio::spawn(messages);
-
-            writes.then(|_| Ok(()))
-        });
+    let cmd_args: Vec<_> = opt.cmd_args.iter().map(|s| s.as_str()).collect();
+    let client = match cmd_args.as_slice() {
+        &["subscribe", stream] => {
+            let stream = EsStream::from_str(stream).unwrap();
+            sub_connect(&addr)
+                .map_err(|_| ())
+                .and_then(|conn| conn.subscribe_to(stream))
+                .and_then(|msgs| msgs.for_each(|msg| {
+                    println!("{:?}", msg);
+                    future::ok(())
+                }))
+        },
+        &["publish", stream, event] => {
+            unimplemented!()
+        }
+        _ => unimplemented!(),
+    };
 
     tokio::run(client);
 }
