64f2b1b6358a5a0a9a6940a79e68d59394037652
Clément Renault <renault.cle@gmail.com>
2019-03-11 16:36:29 +0100

    feat: Use the new to/from_be_bytes methods

--- meilies-server/src/event_id.rs
+++ meilies-server/src/event_id.rs
@@ -1,26 +1,17 @@
-use std::{fmt, mem};
+use std::fmt;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct EventId([u8; 8]);
 
-impl EventId {
-    pub fn from_raw(slice: &[u8]) -> Result<EventId, ()> {
-        if slice.len() != 8 { return Err(()) }
-
-        let ptr = slice.as_ptr() as *const [u8; 8];
-        unsafe { Ok(EventId(*ptr)) }
-    }
-}
-
 impl From<u64> for EventId {
     fn from(id: u64) -> EventId {
-        EventId(unsafe { mem::transmute(id.to_be()) })
+        EventId(id.to_be_bytes())
     }
 }
 
 impl Into<u64> for EventId {
     fn into(self) -> u64 {
-        u64::from_be(unsafe { mem::transmute(self.0) })
+        u64::from_be_bytes(self.0)
     }
 }
 
