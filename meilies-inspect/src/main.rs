7761a1e74df859e8a10df1b82fed60bd4e2f3752
Clément Renault <clement@meilisearch.com>
2019-09-18 18:18:54 +0200

    feat: Port meilies-inspect to the new async/await version

--- meilies-inspect/src/main.rs
+++ meilies-inspect/src/main.rs
@@ -1,9 +1,9 @@
-use std::io::{Write, Error, ErrorKind};
+use std::io::Write;
 use std::net::ToSocketAddrs;
 use std::process::{Command, Stdio};
 
-use futures::future::{self, Future};
-use futures::stream::Stream;
+use futures::executor::ThreadPool;
+use futures::StreamExt;
 use meilies::reqresp::Response;
 use meilies::stream::Stream as EsStream;
 use meilies_client::sub_connect;
@@ -25,7 +25,7 @@ struct Opt {
 
     /// Stream to follow on which events need to be inspected.
     #[structopt(long = "stream", parse(try_from_str))]
-    stream: EsStream,
+    esstream: EsStream,
 
     /// Command and arguments that will interpret the event data piped in stdin.
     ///
@@ -35,14 +35,8 @@ struct Opt {
     command: String,
 }
 
-fn future_io_err<T, E>(error: E) -> future::FutureResult<T, std::io::Error>
-where E: Into<Box<dyn std::error::Error + Send + Sync>>,
-{
-    future::err(Error::new(ErrorKind::Other, error))
-}
-
 fn main() {
-    let Opt { hostname, port, stream, command } = Opt::from_args();
+    let Opt { hostname, port, esstream, command } = Opt::from_args();
 
     let addr = (hostname.as_str(), port);
     let addr = match addr.to_socket_addrs().map(|addrs| addrs.filter(|a| a.is_ipv4()).next()) {
@@ -51,57 +45,52 @@ fn main() {
         Err(e) => panic!("error parsing addr; {}", e),
     };
 
-    let fut = sub_connect(addr)
-            .map_err(|e| eprintln!("{}", e))
-            .and_then(move |(mut ctrl, msgs)| {
-                ctrl.subscribe_to(stream);
-                msgs
-                    .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))
-                    .for_each(move |msg| {
-                        match msg {
-                            Ok(Response::Event { stream, number, event_name, event_data }) => {
-                                eprintln!("processing event number {}", number.0);
+    let pool = ThreadPool::new().unwrap();
 
-                                let result = Command::new("/bin/bash")
-                                    .arg("-c")
-                                    .arg(&command)
-                                    .stdin(Stdio::piped())
-                                    .env("MEILIES_STREAM_NAME",  stream.into_inner())
-                                    .env("MEILIES_EVENT_NAME",   event_name.into_inner())
-                                    .env("MEILIES_EVENT_NUMBER", number.0.to_string())
-                                    .spawn();
+    pool.clone().run(async move {
+        let (mut ctrl, mut stream) = sub_connect(&pool, addr).await.unwrap();
 
-                                let mut child = match result {
-                                    Ok(child) => child,
-                                    Err(e) => return future::err(e),
-                                };
+        ctrl.subscribe_to(esstream).await.unwrap();
 
-                                let data = event_data.0.as_slice();
-                                if let Err(e) = child.stdin.as_mut().unwrap().write_all(data) {
-                                    return future::err(e)
-                                }
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(Ok(Response::Event { stream, number, event_name, event_data })) => {
+                    eprintln!("processing event number {}", number.0);
 
-                                let output = match child.wait_with_output() {
-                                    Ok(output) => output,
-                                    Err(e) => return future::err(e),
-                                };
+                    let result = Command::new("/bin/bash")
+                        .arg("-c")
+                        .arg(&command)
+                        .stdin(Stdio::piped())
+                        .env("MEILIES_STREAM_NAME",  stream.into_inner())
+                        .env("MEILIES_EVENT_NAME",   event_name.into_inner())
+                        .env("MEILIES_EVENT_NUMBER", number.0.to_string())
+                        .spawn();
 
-                                if !output.status.success() {
-                                    return future_io_err("command execution was not successful")
-                                }
+                    let mut child = match result {
+                        Ok(child) => child,
+                        Err(e) => return eprintln!("{}", e),
+                    };
 
-                                future::ok(())
-                            },
-                            Ok(_response) => future::ok(()),
-                            Err(error) => future_io_err(format!("Error: {}", error)),
-                        }
-                    })
-                    .map_err(|e| eprintln!("{}", e))
-            })
-            .and_then(|_| {
-                println!("Connection closed by the server");
-                Err(())
-            });
+                    let data = event_data.0.as_slice();
+                    if let Err(e) = child.stdin.as_mut().unwrap().write_all(data) {
+                        return eprintln!("{}", e);
+                    }
 
-    tokio::run(fut);
+                    let output = match child.wait_with_output() {
+                        Ok(output) => output,
+                        Err(e) => return eprintln!("{}", e),
+                    };
+
+                    if !output.status.success() {
+                        return eprintln!("command execution was not successful")
+                    }
+                },
+                Ok(Ok(otherwise)) => println!("{:?}", otherwise),
+                Ok(Err(e)) => return eprintln!("{}", e),
+                Err(e) => return eprintln!("{}", e),
+            }
+        }
+
+        println!("Connection closed by the server");
+    });
 }
